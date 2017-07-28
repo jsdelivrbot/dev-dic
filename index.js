3f2ad5eb0487f4dc04282c2c650257f020d9bf8e
idouhammou <idouhammou.a@gmail.com>
2017-07-28 15:15:49 +0200

    update index.js [Probléme de port ]

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ app.get('/', function(request, response) {
   response.render('pages/index.twig');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+-app.listen(app.get('port'), function() {
+ -  console.log('Node app is running on port', app.get('port'));
+ +app.get('/', function(request, response) {
+ +  response.render('pages/index.twig');
+  });
