d481c315d1382590a750627f3e34a0937fca68f1
idouhammou <idouhammou.a@gmail.com>
2017-07-28 15:39:14 +0200

    update index.js [Probléme de port ]

--- index.js
+++ index.js
@@ -29,8 +29,18 @@ var db    = mysql.createConnection({
     
 
 app.get('/', function(request, response) {
-  response.render('/pages/index.twig');
+  response.render('pages/index.twig');
 });
 
+/*
+
+*/
 app.listen(app.get('port'), function() {
  console.log('Node app is running on port', app.get('port'));
+
+/*
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+ });
+ 
+*/
